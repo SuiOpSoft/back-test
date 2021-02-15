421dda524894eaf9ee42a4009d20ab633d1099e4
SuiOpSoft <suiopsoft@gmail.com>
2021-02-15 12:17:42 +0000

    Models Companies and Users created

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,61 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Companies(db.Model):
+    __tablename__ = 'companies'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    dateofstablish = db.Column(db.Integer, nullable=True)
+    description = db.Column(db.String(120), nullable=True)
+    address = db.Column(db.String(120), nullable=True)
+    users = db.relationship('Users')
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'<Companies {self.name}>'
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "company_id": self.id,
+            "name": self.name,
+            "dateofstablish": self.name,
+            "description": self.name,
+            "address": self.name,
+            "users": list(map(lambda x: x.serialize(), self.users))
         }
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(80), nullable=False)
+    lastname = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    company_id = db.Column(db.Integer, db.ForeignKey("companies.id"), nullable=False)
+    
+    def __repr__(self):
+        return '<Users {self.name}>'
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email
+        }
+
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
